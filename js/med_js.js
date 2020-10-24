b7fb1aa4202d02435029e711dfd5e2e7dbfec529
VReyes <34307958+issi1489@users.noreply.github.com>
2020-10-24 02:09:55 -0300

    Add files via upload

--- js/med_js.js
+++ js/med_js.js
@@ -1,43 +1,19 @@
 
+/*FUNCIÓN LOGIN */
 
-$('#calendar').fullCalendar({
-    header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay,listWeek'
-    },
-    defaultDate: '2018-11-16',
-    navLinks: true,
-    eventLimit: true,
-    events: [{
-            title: 'Front-End Conference',
-            start: '2018-11-16',
-            end: '2018-11-18'
-        },
-        {
-            title: 'Hair stylist with Mike',
-            start: '2018-11-20',
-            allDay: true
-        },
-        {
-            title: 'Car mechanic',
-            start: '2018-11-14T09:00:00',
-            end: '2018-11-14T11:00:00'
-        },
-        {
-            title: 'Dinner with Mike',
-            start: '2018-11-21T19:00:00',
-            end: '2018-11-21T22:00:00'
-        },
-        {
-            title: 'Chillout',
-            start: '2018-11-15',
-            allDay: true
-        },
-        {
-            title: 'Vacation',
-            start: '2018-11-23',
-            end: '2018-11-29'
-        },
-    ]
-});
+function pasuser(form) {
+    if (form.id.value=="margarita" && form.pass.value=="1234") { 
+                
+        location="privada_paciente.html" 
+      } 
+    
+    else if (form.id.value=="dra.elizabeth" && form.pass.value=="4321") { 
+                
+        location="index.html" 
+      } 
+
+    else {
+        alert("Datos incorrectos, por favor vuelve a intentar")
+      }
+  
+  }
